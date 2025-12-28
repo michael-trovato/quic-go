951917a1b9082d06f8e2029de807eb56926c6e0d
Michael Trovato <mtrovato@trovatocloud.com>
2025-12-28 17:18:07 -0600

    Refactor timeThreshold and packetThreshold types

--- internal/ackhandler/sent_packet_handler.go
+++ internal/ackhandler/sent_packet_handler.go
@@ -18,9 +18,12 @@ import (
 var (
 	// Maximum reordering in time space before time based loss detection considers a packet lost.
 	// Specified as an RTT multiplier.
-	timeThreshold = 9.0 / 8
+	timeThreshold float64 = 9.0 / 8
 	// Maximum reordering in packets before packet threshold loss detection considers a packet lost.
-	packetThreshold = 3
+	packetThreshold protocol.PacketNumber = 3
+)
+
+const (
 	// Before validating the client's address, the server won't send more than 3x bytes than it received.
 	amplificationFactor = 3
 	// We use Retry packets to derive an RTT estimate. Make sure we don't set the RTT to a super low value yet.
